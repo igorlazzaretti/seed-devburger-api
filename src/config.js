3667b4ac0efdbe449b8d4561a8202998395ebff4
rodolfomori <rod_dolpho@hotmail.com>
2025-07-19 10:23:00 -0300

    finalizado

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config = {
-  userToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIwZWE3ZmMyLTJjYTUtNGQxNi05NDBhLTNlMzkyZjE0MTdmYiIsIm5hbWUiOiJBZ3VzdGluaG8gTmV0byIsImlhdCI6MTcyMTMxOTg0NSwiZXhwIjoxNzIxNzUxODQ1fQ.93xLVuBQwPOWbkCAIzhe3FiEJSIAj37C5h3k4ZWPB_k',
+  userToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImZkYzgwMzllLTA2ZjAtNGMyYi1iYmMwLWE1ZWNjZDE1OWQ4MCIsIm5hbWUiOiJSb2RvbGZvIiwiaWF0IjoxNzIxMzMzMjY2LCJleHAiOjE3MjE3NjUyNjZ9.Qt9jFtmU8RqLuV2chAdAoqWsssXxBQSo226Ohrq8FS8',
   apiUrl: 'http://localhost:3001'
 }
